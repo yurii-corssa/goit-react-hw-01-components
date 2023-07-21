747e10071326a882a90bd2192a1dd0c3a4b0c3ab
yurii-corssa <yurii.corssa@gmail.com>
2023-07-21 21:54:03 +0200

    create friend list card

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import friends from '../../friends.json';
+
+export const FriendList = () => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <li className="item" key={id}>
+            <span className="status"></span>
+            <img className="avatar" src={avatar} alt={name} width="48" />
+            <p className={name}>{name}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
